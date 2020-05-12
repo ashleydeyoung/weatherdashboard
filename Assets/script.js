dd686f587e4d990f27d3c4d215d3b02b3587c7e8
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-05-11 22:47:09 -0400

    jquery edits

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
-var searchInput = document.getElementById("searchtext");
-var submitBtn = document.getElementById("submit-btn");
-var searchList = document.getElementById("search-list");
+var searchInput = $("#searchtext");
+var submitBtn = $("#submit-btn");
+var searchList = $("#search-list");
 
 var searchArray = [];
 
@@ -8,17 +8,32 @@ init();
 
 function renderSearch() {
   // Clear todoList element and update todoCountSpan
-  searchList.innerHTML = "";
+  searchList.empty();
 
-  // Render a new li for each todo
+  // Render a new li for each list
+  // $.each(searchArray, function(i, search) {
+  //   searchList.append("<div>" + search + "</div>");
+  // });
+
   for (var i = 0; i < searchArray.length; i++) {
-    var search = searchArray[i];
 
-    var li = document.createElement("li");
-    li.textContent = search;
-    // li.setAttribute("data-index", i);
-    searchList.appendChild(li);
+    // It then creates a new div for each drink. Note we create divs and add the content in the same line.
+    var newSearchDiv = $("<div>")
+    newSearchDiv.addClass("search");
+    newSearchDiv.html(searchArray[i])
+    // It then adds this new div to the search div.
+    searchList.append(newSearchDiv);
   }
+  
+ 
+  // for (var i = 0; i < searchArray.length; i++) {
+  //   var search = searchArray[i];
+
+  //   var li = document.createElement("li");
+  //   li.textContent = search;
+  //   // li.setAttribute("data-index", i);
+  //   searchList.appendChild(li);
+  // }
 }
 
 
@@ -26,12 +41,12 @@ function init() {
   // Get stored search from localStorage
   // Parsing the JSON string to an object
   var storedSearch = JSON.parse(localStorage.getItem("search"));
-
+  
   // If search were retrieved from localStorage, update the search array to it
   if (storedSearch !== null) {
     searchArray = storedSearch;
   }
-
+  console.log(searchArray)
   // Render search to the DOM
   renderSearch();
 }
@@ -42,11 +57,11 @@ function storeSearch() {
 }
 
 // When form is submitted...
-$(submitBtn.addEventListener("click", function(event) {
+$(submitBtn).click( function(event) {
   event.preventDefault();
 
-  var searchText = searchInput.value.trim();
-  console.log(searchText);
+  var searchText = searchInput.val().trim();
+  // console.log(searchText);
 
   // Return from function early if submitted Text is blank
   if (searchText === "") {
@@ -60,6 +75,6 @@ $(submitBtn.addEventListener("click", function(event) {
   // Store updated search in localStorage, re-render the list
   storeSearch();
   renderSearch();
-}));
+});
 
-// function for when city is clicked
+// Create function for when city is clicked
