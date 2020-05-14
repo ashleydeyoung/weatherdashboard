ae91f0084d781068395b09c6f7dbea925870e227
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-05-13 21:19:32 -0400

    uv index color, edit comments section

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,7 @@ var searchInput = $("#searchtext");
 var submitBtn = $("#submit-btn");
 var searchList = $("#search-list");
 
+//searchArray for search history
 var searchArray = [];
 
 init();
@@ -16,42 +17,54 @@ function displayWeatherInfo() {
   var queryURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + search  + "&units=imperial&appid=a433785d6c40d7591842a50a08b4a776"
 
 
-  // Creates AJAX call for the specific movie button being clicked
+  // Creates AJAX call for current weather
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
    console.log(response)
-    // Creates a div to hold the movie
+    // Creates a div to hold the current weather
     var newDiv = $("<div>")
     newDiv.addClass("search-div")
     newDiv.prependTo("#result-page")
 
     $("<h1>").addClass("current-city").text(response.name).appendTo(newDiv)
     $("<h2>").text("Current Temp: " + Math.round(response.main.temp) + " \xB0 F").appendTo(newDiv)
- 
     $("<p>").text("Humidity: " + response.main.humidity + "%").appendTo(newDiv)
-  
     $("<p>").text("Wind: " + response.wind.speed + " MPH").appendTo(newDiv)
-   
     $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png").appendTo(".current-city")
 
    lat = response.coord.lat;
    lon = response.coord.lon;
 
    var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=a433785d6c40d7591842a50a08b4a776&lat=" + lat + "&lon=" +lon
+   //creates AJAX call for UV index
    $.ajax({
      url: queryURL,
      method: "GET"
    }).then(function(response) {
-    console.log(response)
-    $("<p>").text("UV Index: " + response.value ).appendTo(newDiv)
-   })
-    
+    console.log(JSON.parse(response.value))
+    $("<p>").addClass("uv-index").text("UV Index: ").appendTo(newDiv)
+    $("<button>").addClass("uv-button").appendTo((".uv-index"))
+    //color change based on uv index value
+    if (JSON.parse(response.value) > 7) {
+      $(".uv-button").css("background-color", "red").text(response.value) 
+    }; 
+    if (JSON.parse(response.value) < 4) {
+      $(".uv-button").css("background-color", "green").text(response.value) 
+
+    }; 
+    if (JSON.parse(response.value) >=4 && JSON.parse(response.value <=7)) {
+      $(".uv-button").css("background-color", "yellow").text(response.value) 
+   
+    }
+   }
+   )
+  
   });
 
   
-
+  //AJAX for 5 day forecast
   var search = searchArray[searchArray.length -1]
   var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&units=imperial&appid=a433785d6c40d7591842a50a08b4a776"
   $.ajax({
@@ -73,12 +86,7 @@ function displayWeatherInfo() {
       $("<p>").text("Temp: " + Math.round(response.list[randomArray[i]].main.temp) + "\xB0  F").appendTo("#day" + i )
       $("<p>").text("Humidity: " + response.list[randomArray[i]].main.humidity + "%").appendTo("#day" + i )
       
-      
-      
     }
-
-  
-
   });
 
 }
@@ -87,10 +95,6 @@ function renderSearch() {
   // Clear List element 
   searchList.empty();
 
-  // Render a new li for each list
-  // $.each(searchArray, function(i, search) {
-  //   searchList.append("<div>" + search + "</div>");
-  // });
 
   for (var i = 0; i < searchArray.length; i++) {
 
@@ -98,19 +102,10 @@ function renderSearch() {
     var newSearchDiv = $("<div>")
     newSearchDiv.addClass("search").attr("data-name", searchArray[i]);
     newSearchDiv.html(searchArray[i])
-    // It then adds this new div to the search div.
+    //adds this new div to the search div.
     searchList.prepend(newSearchDiv);
   }
   
- 
-  // for (var i = 0; i < searchArray.length; i++) {
-  //   var search = searchArray[i];
-
-  //   var li = document.createElement("li");
-  //   li.textContent = search;
-  //   // li.setAttribute("data-index", i);
-  //   searchList.appendChild(li);
-  // }
 }
 
 
@@ -123,7 +118,7 @@ function init() {
   if (storedSearch !== null) {
     searchArray = storedSearch;
   }
-  // console.log(searchArray)
+  
   // Render search to the DOM
   renderSearch();
   displayWeatherInfo()
@@ -142,8 +137,7 @@ $("#search-form").on("submit", function (event) {
   $("#forecast").empty();
 
   var searchText = searchInput.val().trim();
-  // console.log(searchText);
-
+  
   // Return from function early if submitted Text is blank
   if (searchText === "") {
     return;
@@ -168,66 +162,7 @@ $(document).on("click", ".search", function searchClick() {
   renderSearch();
   displayWeatherInfo();
  
-  // var search = $(this).attr("data-name")
-
- 
-  // var queryURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + search  + "&units=imperial&appid=a433785d6c40d7591842a50a08b4a776"
-
-
-  // // Creates AJAX call for the specific movie button being clicked
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).then(function(response) {
-  //  console.log(response)
-  //   // Creates a div to hold the movie
-  //   var newDiv = $("<div>")
-  //   newDiv.addClass("search-div")
-  //   newDiv.prependTo("#result-page")
-
-  //   $("<h1>").addClass("current-city").text(response.name).appendTo(newDiv)
-  //   $("<h2>").text("Current Temp: " + Math.round(response.main.temp) + " \xB0 F").appendTo(newDiv)
- 
-  //   $("<p>").text("Humidity: " + response.main.humidity + "%").appendTo(newDiv)
   
-  //   $("<p>").text("Wind: " + response.wind.speed + " MPH").appendTo(newDiv)
-   
-  //   $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png").appendTo(".current-city")
-    
-    
-  // });
-
-  // var search = searchInput.val().trim()
-  // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&units=imperial&appid=a433785d6c40d7591842a50a08b4a776"
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).then(function(response) {
-  //  console.log(response)
-
-  
-  //   console.log(response.list[0].dt_txt);
-
-  //   randomArray= [3, 11, 19, 27, 35];
-
-  //   for (var i = 0; i < randomArray.length; i++) {
-  //     console.log(i)
-     
-  //     $("<div>").addClass("col fiveday").attr('id', 'day' + i).html("<h6>" + (response.list[randomArray[i]].dt_txt) + "</h4>").appendTo("#forecast");
-  //     $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.list[randomArray[i]].weather[0].icon + "@2x.png").appendTo("#day" + i )
-  //     $("<p>").text("Temp: " + Math.round(response.list[randomArray[i]].main.temp) + "\xB0  F").appendTo("#day" + i )
-  //     $("<p>").text("Humidity: " + response.list[randomArray[i]].main.humidity + "%").appendTo("#day" + i )
-      
       
       
 });
-// // Create function for when city is clicked
-
-// $(document).on("click", ".search", searchClick())
-
-// function searchClick() {
-//   $(".search-div").empty();
-//   $("#forecast").empty();
-//   searchInput = $(this).attr("data-name");
-
-// })});
